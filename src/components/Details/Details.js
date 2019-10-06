9181478b2811a4777f81862731ae0e935225a5dd
David Diaz <daemdisa@aol.com>
2019-10-06 00:56:59 -0500

    menu icon is made want to see if i can work with it

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Button from "@material-ui/core/Button";
 
+// import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+// const useStyles = makeStyles(theme => ({
+//     root: {
+//       flexGrow: 1,
+//     },
+//     menuButton: {
+//       marginRight: theme.spacing(2),
+//     },
+//     title: {
+//       flexGrow: 1,
+//     },
+//   }));
+//   const classes = useStyles();
+
 
 class Details extends Component {
 
@@ -40,7 +65,25 @@ class Details extends Component {
             return (<ul key={movie.id}><li><span>{movie.name}</span></li></ul>)
         }) // end map
         return (
+
             <div className="description">
+                        <>
+                        <AppBar position="static">
+                        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          {/* <Typography variant="h6" >
+            News
+          </Typography> */}
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+      </>
             <Button
               variant="contained"
               color="primary"
@@ -60,6 +103,7 @@ class Details extends Component {
                 {movieDescription}
                 {filmInfo}
             </div>
+
         ); // end return
     } // end render
 } // end Details component
