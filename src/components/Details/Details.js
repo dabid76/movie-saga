76ec1b05c4f04f9d9a0d362887532754e35f5d7d
David Diaz <daemdisa@aol.com>
2019-10-06 12:28:23 -0500

    menu btn not working

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,32 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Button from "@material-ui/core/Button";
 
-// import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-
-
-
-// const useStyles = makeStyles(theme => ({
-//     root: {
-//       flexGrow: 1,
-//     },
-//     menuButton: {
-//       marginRight: theme.spacing(2),
-//     },
-//     title: {
-//       flexGrow: 1,
-//     },
-//   }));
-//   const classes = useStyles();
-
-
 class Details extends Component {
 
     state = {
@@ -67,23 +41,6 @@ class Details extends Component {
         return (
 
             <div className="description">
-                        <>
-                        <AppBar position="static">
-                        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          {/* <Typography variant="h6" >
-            News
-          </Typography> */}
-          {/* <Button color="inherit">Login</Button> */}
-        </Toolbar>
-      </AppBar>
-      </>
             <Button
               variant="contained"
               color="primary"
